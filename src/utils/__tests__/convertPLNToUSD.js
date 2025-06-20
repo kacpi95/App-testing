f577d8ea295835b1c5449f11557670bf5ab4f8a4
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-06-20 13:01:03 +0200

    Add second and third test

---
+++ src/utils/__tests__/convertPLNToUSD.js
@@ -0,0 +1,20 @@
+import { convertPLNToUSD } from './../convertPLNToUSD';
+
+describe('ConvertPLNtoUSD', () => {
+  it('should return proper value when good input', () => {
+    expect(convertPLNToUSD(1)).toBe('$0.29');
+    expect(convertPLNToUSD(2)).toBe('$0.57');
+    expect(convertPLNToUSD(20)).toBe('$5.71');
+    expect(convertPLNToUSD(12)).toBe('$3.43');
+  });
+
+  it('should return NAN when input is text', () => {
+    expect(convertPLNToUSD('1')).toBeNaN();
+    expect(convertPLNToUSD('abc')).toBeNaN();
+    expect(convertPLNToUSD('1f3')).toBeNaN();
+  });
+
+  it('should return NAN when input is empty', () => {
+    expect(convertPLNToUSD()).toBeNaN();
+  });
+});
--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,11 +1,14 @@
 export const convertPLNToUSD = (PLN) => {
+  if (typeof PLN !== 'number') {
+    return NaN;
+  }
 
   const PLNtoUSD = PLN / 3.5;
-  
+
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
-    currency: 'USD'
+    currency: 'USD',
   });
 
   return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
-}
+};
